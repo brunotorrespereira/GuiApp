27ba62a1d09dd1d3577e9e2600d09c9bcb40e750
Bruno_Pereira <w.brunopereiraa@gmail.com>
2024-02-23 11:41:16 -0500

    primeiro commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, StyleSheet,Image } from "react-native";
+
+
+export default function App(){
+  return(
+  <View style={StyleSheet.container}>
+
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'#00aeef'
+  }
+})
